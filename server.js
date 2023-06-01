4c1419fa513d8554673cc44a5f575d8a59368d83
nittiku <nittk986@gmail.com>
2023-06-02 01:31:26 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+// Serve static files from the 'src' directory
+app.use(express.static("src"));
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
